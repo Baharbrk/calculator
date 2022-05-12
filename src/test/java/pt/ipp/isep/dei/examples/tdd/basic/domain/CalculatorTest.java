4e12b6f2be253ddb662281b319c139002b8f6a25
Baharebrk <alikhaz@hotmail.com>
2022-05-13 01:07:35 +0200

    calculator tests

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/CalculatorTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/CalculatorTest.java
@@ -95,6 +95,406 @@ public class CalculatorTest {
         // Assert
         assertEquals(expectedResult, result);
     }
+
+    /**
+     * Test for two positive operands
+     **/
+    @Test
+    public void ensureFourPlusTwoIsSix() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 2;
+        int expectedResult = 6;
+        int result = 0;
+
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two negative operands
+     **/
+    @Test
+    public void ensureMinusFourPlusMinusTwoIsMinusSix() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = -2;
+        int expectedResult = -6;
+        int result = 0;
+
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one positive operand and one negative one when positive operand is bigger than the negative one
+     **/
+    @Test
+    public void ensureFourPlusMinusTwoIsTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = -2;
+        int expectedResult = 2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one positive operand and one negative one when negative operand is bigger than the positive one
+     **/
+    @Test
+    public void ensureMinusFourPluTwoIsMinusTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = 2;
+        int expectedResult = -2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one positive operand and one negative one when negative operand is bigger than the positive one
+     **/
+    @Test
+    public void ensureFourPlusZeroIsFour() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 0;
+        int expectedResult = 4;
+        int result = 0;
+
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two positive operands when first operand is bigger than the second one
+     **/
+    @Test
+    public void ensureFourSubTwoIsTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 2;
+        int expectedResult = 2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().subtract(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two positive operands when first operand is smaller than the second one
+     **/
+    @Test
+    public void ensureTwoSubFourIsMinusTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 2;
+        int secondOperand = -4;
+        int expectedResult = -2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().subtract(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two negative numbers
+     **/
+    @Test
+    public void ensureMinusFourSubMinusTwoIsTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = -2;
+        int expectedResult = 2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().subtract(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one positive operand and one negative one
+     **/
+    @Test
+    public void ensureTwoSubMinusFourIsSix() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 2;
+        int secondOperand = -4;
+        int expectedResult = 6;
+        int result = 0;
+
+        // Act
+        result = new Calculator().subtract(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one number with 0
+     **/
+    @Test
+    public void ensureFourSubZeroIsFour() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 0;
+        int expectedResult = 4;
+        int result = 0;
+
+        // Act
+        result = new Calculator().subtract(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two positive operands
+     **/
+    @Test
+    public void ensureFourMultipliesTwoIsEight() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 2;
+        int expectedResult = 8;
+        int result = 0;
+
+        // Act
+        result = new Calculator().multiply(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two negative numbers
+     **/
+    @Test
+    public void ensureMinusFourMultipliesMinusTwoIsEight() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = -2;
+        int expectedResult = 8;
+        int result = 0;
+
+        // Act
+        result = new Calculator().multiply(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one positive operand and one negative one
+     **/
+    @Test
+    public void ensureMinusFourMultipliesTwoIsMinusEight() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = 2;
+        int expectedResult = -8;
+        int result = 0;
+
+        // Act
+        result = new Calculator().multiply(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one operand with 0
+     **/
+    @Test
+    public void ensureFourMultipliesZeroIsZeo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 0;
+        int expectedResult = 0;
+        int result = 0;
+
+        // Act
+        result = new Calculator().multiply(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two positive operands
+     **/
+    @Test
+    public void ensureFourDividedByTwoIsTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 2;
+        int expectedResult = 2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().divide(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for two negative numbers
+     **/
+    @Test
+    public void ensureMinusFourDividedByMinusTwoIsTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = -2;
+        int expectedResult = 2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().divide(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for one positive operand and one negative one
+     **/
+    @Test
+    public void ensureMinusFourDividedByTwoIsMinusTwo() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = -4;
+        int secondOperand = 2;
+        int expectedResult = -2;
+        int result = 0;
+
+        // Act
+        result = new Calculator().divide(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for  0 as first operand
+     **/
+    @Test
+    public void ensureZeroDividedByFourIsZero() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 0;
+        int secondOperand = 4;
+        int expectedResult = 0;
+        int result = 0;
+
+        // Act
+        result = new Calculator().divide(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test for  0 as second operand
+     **/
+    @Test
+    public void ensureFourDividedByZeroThrowsError() {
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+
+        // Arrange
+        int firsOperand = 4;
+        int secondOperand = 0;
+        int expectedResult = Error;
+        int result = 0;
+
+        // Act
+        result = new Calculator().divide(firsOperand, secondOperand);
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
 }
 
 
